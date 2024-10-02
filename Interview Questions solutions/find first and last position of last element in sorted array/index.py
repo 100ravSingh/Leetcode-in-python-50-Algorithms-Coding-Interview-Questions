477d5fc501429a5de55c8de301d58b71f57a65d5
Sourav Singh <55342631+100ravSingh@users.noreply.github.com>
2024-10-02 18:17:34 +0530

    Update index.py
    Check this one, short code and passed all leetcode testcases. You can try.

--- Interview Questions solutions/find first and last position of last element in sorted array/index.py
+++ Interview Questions solutions/find first and last position of last element in sorted array/index.py
@@ -1,40 +1,21 @@
 class Solution:
-    def getLeftPosition(self, nums, target):
+    def searchRange(self, A: List[int], target: int) -> List[int]:
         left = 0
-        right = len(nums)-1
+        right = len(A)
+        ans = []
+        
+        if len(A) == 0:
+            return [-1,-1]
 
-        while(left <= right):
-            mid = left+(right-left)//2
-            if(nums[mid] == target):
-                if(mid-1 >= 0 and nums[mid-1] != target or mid == 0):
-                    return mid
-                right = mid-1
-            elif(nums[mid] > target):
-                right = mid-1
-            else:
-                left = mid+1
+        if target not in A:
+            return [-1,-1]
 
-        return -1
+        while(left < right):
+            if A[left] == target:
+                ans.append(left)
+            left += 1
 
-    def getRightPosition(self, nums, target):
-        left = 0
-        right = len(nums)-1
-
-        while(left <= right):
-            mid = left+(right-left)//2
-            if(nums[mid] == target):
-                if(mid+1 < len(nums) and nums[mid+1] != target or mid == len(nums)-1):
-                    return mid
-                left = mid+1
-            elif(nums[mid] > target):
-                right = mid-1
-            else:
-                left = mid+1
-
-        return -1
-
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
-        left = self.getLeftPosition(nums, target)
-        right = self.getRightPosition(nums, target)
-
-        return [left, right]
+        if len(ans) < 2:
+            ans.append(ans[0])
+    
+        return [ans[0],ans[-1]]
